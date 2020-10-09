2814d0d8385149894e3aacae4f5d139fc79a3250
Ashish Tyagi <ashisht@dewsolutions.in>
2020-10-09 16:28:06 +0530

    Initial Commit

---
+++ SwiftUI Widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Widget
+//
+//  Created by Ashish Tyagi on 09/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Widget/SwiftUI_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WidgetApp.swift
+//  SwiftUI Widget
+//
+//  Created by Ashish Tyagi on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
